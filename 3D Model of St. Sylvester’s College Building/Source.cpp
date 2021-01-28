805ddae02cda44d2c09d495213c761a7a86be174
ravdsn <ravindu.dilshan.rd@gmail.com>
2021-01-28 11:42:36 +0530

    configure project by adding requred opengl files | implement main components in the source.cpp file

---
+++ 3D Model of St. Sylvester’s College Building/Source.cpp
@@ -0,0 +1,66 @@
+#include <iostream>
+#include <GL/glut.h>
+
+GLfloat x = 6.0f;
+GLfloat y = 6.0f;
+GLfloat z = 6.0f;
+
+void init() {
+    glClearColor(0.0f, 0.0f, 0.0f, 1.0f);
+    glEnable(GL_DEPTH_TEST);                // Enable the Depth testig in openGL
+    glEnable(GL_CULL_FACE);                 // Enable Hidden Surface Removal
+    glPolygonMode(GL_FRONT, GL_FILL);       // Set the Front of the faces of the cube to be unfilled (Skeleton Model)
+    glPolygonMode(GL_BACK, GL_FILL);        // Set the Back of the faces of the cube to be filled with the color
+
+}
+
+void display() {
+    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);      // Depth Buffer should be cleared everytime a redraw happens
+
+    glPushMatrix();                                //
+    glRotatef(-30.0f, 1.0f, 0.0f, 0.0f);          //      Rotation of the Coordinate space
+    glRotatef(30.0f, 0.0f, 1.0f, 0.0f);           //
+
+    
+
+    glPopMatrix();                 
+    glutSwapBuffers();
+}
+
+void changeSize(GLsizei w, GLsizei h) {
+    glViewport(0, 0, w, h);
+
+    GLfloat aspect_ratio = h == 0 ? w / 1 : (GLfloat)w / (GLfloat)h;
+
+    glMatrixMode(GL_PROJECTION);
+    glLoadIdentity();
+
+    if (w <= h)
+        glOrtho(-10.0, 10.0, -10 / aspect_ratio, 10 / aspect_ratio, 10.0, -10.0);
+    else
+        glOrtho(-10.0 * aspect_ratio, 10.0 * aspect_ratio, -10.0, 10.0, 10.0, -10.0);
+
+    glMatrixMode(GL_MODELVIEW);
+    glLoadIdentity();
+
+
+}
+
+int main(int argc, char** argv) {
+
+    glutInit(&argc, argv);
+    glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGBA | GLUT_DEPTH);
+    glutInitWindowSize(320, 320);
+    glutInitWindowPosition(150, 150);
+    glutCreateWindow("St. Sylvester's College Building");
+    glutDisplayFunc(display);
+    glutReshapeFunc(changeSize);
+    init();
+    glutMainLoop();
+
+
+    return 0;
+}
+
+
+
